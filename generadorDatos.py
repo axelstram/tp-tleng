31d48fd8ef7aa978953c56ace3b669bb10d431b0
fzanollo <fzanollo@dc.uba.ar>
2019-07-30 00:37:01 -0300

    detalles

--- generadorDatos.py
+++ generadorDatos.py
@@ -1,83 +1,79 @@
 import random
 import string
 import json
-# aux
 
-def randomString(stringLength=5):
+def generateString():
+    stringLength = random.randint(4, 10)
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(stringLength))
 
-# generadores
-def generateString():
-	return '\"' +  randomString() + '\"'
-
 def generateInt():
-	return random.sample(range(1,100),1)[0]
+    return random.sample(range(1,100),1)[0]
 
 def generateFloat():
-	return round(random.uniform(1,100), 2)
+    return round(random.uniform(1,100), 2)
 
 def generateBool():
-	return bool(random.getrandbits(1))
+    return bool(random.getrandbits(1))
 
 def generateBasicType(tipo):
-	value = None
-	if tipo == 'INT':
-		value = generateInt()
-	elif tipo == 'STRING':
-		value = generateString()
-	elif tipo == 'BOOL':
-		value = generateBool()
-	elif tipo == 'FLOAT':
-		value = generateFloat()
-	else:
-		raise Exception("ERROR, {} NO ES UN TIPO BASICO".format(tipo))
+    value = None
+    if tipo == 'INT':
+        value = generateInt()
+    elif tipo == 'STRING':
+        value = generateString()
+    elif tipo == 'BOOL':
+        value = generateBool()
+    elif tipo == 'FLOAT':
+        value = generateFloat()
+    else:
+        raise Exception("ERROR, {} NO ES UN TIPO BASICO".format(tipo))
 
-	return value
+    return value
 
 def generateArray(elem, definitions):
-	dimensions = 0
-	
-	size = random.randint(1,5)
-	res = [generateValue(elem['type'], definitions) for i in range(size)]
+    dimensions = 0
 
-	return res
+    size = random.randint(1,5)
+    res = [generateValue(elem['type'], definitions) for i in range(size)]
+
+    return res
 
 def isArray(elem):
-	return elem['type']['is_array']
+    return elem['type']['is_array']
 
 def isStruct(elem):
-	return type(elem['type']['type']) == list
+    return type(elem['type']['type']) == list
 
 def isBasicType(elem):
-	return (elem['type']['type'] in ['INT', 'STRING', 'BOOL', 'FLOAT'])
+    return (elem['type']['type'] in ['INT', 'STRING', 'BOOL', 'FLOAT'])
 
 def generateValue(elem, definitions):
-	value = None
+    value = None
 
-	if isArray(elem):
-		value = generateArray(elem, definitions)
-	elif isStruct(elem):
-		value = generateStruct(elem['type']['type'], definitions)
-	elif isBasicType(elem):
-		value = generateBasicType(elem['type']['type'])
-	else: 
-		#es un nuevo tipo de datos (i.e. struct)
-		value = generateStruct(definitions[elem['type']['type']], definitions)
+    if isArray(elem):
+        value = generateArray(elem, definitions)
+    elif isStruct(elem):
+        value = generateStruct(elem['type']['type'], definitions)
+    elif isBasicType(elem):
+        value = generateBasicType(elem['type']['type'])
+    else: 
+        #es un nuevo tipo de datos (i.e. struct)
+        value = generateStruct(definitions[elem['type']['type']], definitions)
 
-	return value
+    return value
 
 def generateStruct(camposDelStruct, definitions):
-	structAsDict = {}
+    structAsDict = {}
 
-	for elem in camposDelStruct:
-		structAsDict[elem['id']] = generateValue(elem, definitions)
+    for elem in camposDelStruct:
+        structAsDict[elem['id']] = generateValue(elem, definitions)
 
-	return structAsDict
+    return structAsDict
 
 def generate_json(structs):
-	definitions = dict([(s['id'], s['structBody']) for s in structs])
-	mainStructAsDict = generateStruct(structs[0]['structBody'], definitions)
+    definitions = dict([(s['id'], s['structBody']) for s in structs])
+    mainStructAsDict = generateStruct(structs[0]['structBody'], definitions)
 
-	return mainStructAsDict
+    return mainStructAsDict
 
